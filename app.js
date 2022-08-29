3c694ae38fc08c63d7b2de1d9ebdec870f0fa29d
Elliot <elliotcs30@gmail.com>
2022-08-30 00:04:19 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -56,6 +56,14 @@ app.post('/records', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.get('/records/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then(record => res.render('detail', { record }))
+    .catch(error => console.log(error))
+})
+
 // 設定 port 3000
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
