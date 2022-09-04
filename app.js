5b8ba406f144263d92685582ed80652c5489aec4
Elliot <elliotcs30@gmail.com>
2022-09-04 21:03:00 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
 
+// 如果在 Heroku 環境則使用 process.env.PORT
+// 否則為本地環境，使用 3000 
+const PORT = process.env.PORT || 3000
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -54,6 +58,6 @@ app.use((req, res, next) => {
 app.use(routes)
 
 // 設定 port 3000
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+app.listen(PORT, () => {
+  console.log(`App is running on http://localhost:${PORT}`)
 })
