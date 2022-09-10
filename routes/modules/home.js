d47bc3dc370011288291eace34322a96430214f8
Elliot <elliotcs30@gmail.com>
2022-09-10 08:46:59 +0800

    feat: modify sort page category show sort

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -56,6 +56,7 @@ router.get('/sort', (req, res) => {
     Categorys
     .find()
     .lean()
+    .sort({ _id: 'asc' }) // desc
     .then(categorys => {
       Promise.all([
         records.map(record => {
